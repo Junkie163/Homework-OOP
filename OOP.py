5129b3f83902413ef8b3c0acde10e2ec6e080830
junkie63 <bss443901.il@gmail.com>
2023-09-29 21:34:49 +0300

    Part3.1

--- OOP.py
+++ OOP.py
@@ -11,7 +11,8 @@ class Student:
         self.finished_courses.append(course_name)
 
     def rate_hw(self, lecturer, course, grade):
-        if isinstance(lecturer, Lecturer) and course in self.courses_in_progress:# and course in student.courses_in_progress:
+        if isinstance(lecturer,
+                      Lecturer) and course in self.courses_in_progress:  # and course in student.courses_in_progress:
             if course in lecturer.grades:
                 lecturer.grades[course] += [grade]
             else:
@@ -31,7 +32,7 @@ class Mentor:
 class Lecturer(Mentor):
     def __init__(self, name, surname, courses_attached):
         super().__init__(name, surname, courses_attached)
-        self.grades = {}
+        self.grade
 
 
 class Reviewer(Mentor):
@@ -46,6 +47,9 @@ class Reviewer(Mentor):
                 student.grades[course] = [grade]
         else:
             return 'Ошибка'
+
+    def __str__(self):
+        return f'Имя: {self.name}' + '\n' + f'Фамилия: {self.surname}'
 
 
 best_student = Student('Ruoy', 'Eman', 'your_gender')
@@ -71,6 +75,7 @@ best_student.rate_hw(cool_lecturer, 'Python', 7)
 best_student.rate_hw(cool_lecturer, 'Git', 8)
 best_student.rate_hw(cool_lecturer, 'Python', 7)
 
-print(cool_mentor.courses_attached)
-print(cool_lecturer.courses_attached)
-print(cool_lecturer.grades)
+#print(cool_mentor.courses_attached)
+#print(cool_lecturer.courses_attached)
+#print(cool_lecturer.grades)
+print(cool_reviewer)
